8a0510523232b3ec8369cc6f9f05e6adcd414c74
judge2020 <hunterray55@hotmail.com>
2017-06-02 06:47:07 -0700

    Initial Commit

---
+++ Altis/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Altis
+//
+//  Created by Hunter Ray on 6/2/17.
+//  Copyright © 2017 Judge2020. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
